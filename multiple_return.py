9ecd97dfa00161c91d0b1a00219d4e8b59b9ad2b
Wei.Liu <xiruanliuwei@163.com>
2017-12-24 06:59:13 -0800

    Add multiple return sample

---
+++ multiple_return.py
@@ -0,0 +1,13 @@
+
+def sum_diff(x, y):
+	sum = x + y
+	diff = x - y
+	return sum, diff
+
+s, d = sum_diff(3, 5)
+print("s is: ", s)
+print("d is: ", d)
+# print(type(s,d)) #it is incorrect
+print(type(s))
+print(type(d))
+
